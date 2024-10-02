2819c962ff36bc080d040019d48c4970b1a16315
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-02 11:41:30 +0900

    부트스트랩 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BootStrapButton from './BootstrapExample';
+import Header from './components/Header';
 
 function App() {
   const hello = "안녕하세요";
@@ -11,8 +12,10 @@ function App() {
 
   return (
     <>
+      <Header></Header>
       <div style={style}>{hello}</div>
       <div>{hello}</div>
+      <BootStrapButton/>
     </>
   )
 }
---
+++ src/BootstrapExample.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+
+function BootStrapButton() {
+
+  return (
+    <>
+      <div>
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="success">Success</Button>
+          <Button variant="warning">Warning</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="info">Info</Button>
+          <Button variant="light">Light</Button>
+          <Button variant="dark">Dark</Button>
+          <Button variant="link">Link</Button>
+      </div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+      <button type="button" class="btn btn-link">Link</button>
+    </>
+  )
+}
+
+export default BootStrapButton
