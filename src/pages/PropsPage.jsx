73af4b7c54573a220364933e0c3eaf0a08cf5dc5
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-04 10:48:13 +0900

    프롭스 완료

--- src/pages/PropsPage.jsx
+++ src/pages/PropsPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hello from '../components/Hello'
+import Hello2 from '../components/Hello2'
 
 const PropsPage = () => {
   return (
@@ -8,8 +9,8 @@ const PropsPage = () => {
     <>
       <Hello name = "홍길동" color = "red" age = "30">
       </Hello>
-      <Hello name = "홍길순" color = "blue" age = "25">
-      </Hello>
+      <Hello2 name = "홍길순" color = "blue" age = "25">
+      </Hello2>
     </>
   )
 }
