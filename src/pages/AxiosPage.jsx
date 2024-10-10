b6ab8854a6b0d0b8fdb63c4f19da94427b116049
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-10-10 10:15:46 +0900

    axios-get-complete

---
+++ src/pages/AxiosPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AxiosGet from '../components/AxiosGet'
+
+const AxiosPage = () => {
+  return (
+    <div className="text-center mt-5">
+      <AxiosGet></AxiosGet>
+    </div>
+  )
+}
+
+export default AxiosPage
